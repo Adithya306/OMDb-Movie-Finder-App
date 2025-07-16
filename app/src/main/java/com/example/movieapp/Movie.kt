1acf24be80cadb6d0e00b8fe5198a36ebc54edae
Adithya306 <garukaadithya@gmail.com>
2025-07-16 14:16:02 +0530

    implement movie class

--- app/src/main/java/com/example/movieapp/Movie.kt
+++ app/src/main/java/com/example/movieapp/Movie.kt
@@ -1,4 +1,20 @@
 package com.example.movieapp
 
-class Movie {
-}
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movies")
+data class Movie(
+    @PrimaryKey val imdbID: String,
+    val Title: String,
+    val Year: String,
+    val Rated: String,
+    val Released: String,
+    val Runtime: String,
+    val Genre: String,
+    val Director: String,
+    val Writer: String,
+    val Actors: String,
+    val Plot: String
+)
