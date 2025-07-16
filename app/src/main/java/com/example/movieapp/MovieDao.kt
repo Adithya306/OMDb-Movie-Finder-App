c48e6dd8d9a6a991496257ea40cdd53d08113b7a
Adithya306 <garukaadithya@gmail.com>
2025-07-16 14:17:08 +0530

    implement moviedao class

--- app/src/main/java/com/example/movieapp/MovieDao.kt
+++ app/src/main/java/com/example/movieapp/MovieDao.kt
@@ -1,4 +1,23 @@
 package com.example.movieapp
 
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
 interface MovieDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovies(movies: List<Movie>)
+
+//    @Query("SELECT * FROM movies")
+//    suspend fun getAllMovies(): List<Movie>
+
+    @Query("SELECT * FROM movies WHERE Title = :title COLLATE NOCASE LIMIT 1")
+    suspend fun getMovieByTitle(title: String): Movie?
+
+    @Query("SELECT * FROM movies WHERE Actors LIKE :actorName")
+    suspend fun getMoviesByActor(actorName: String): List<Movie>
+
 }
